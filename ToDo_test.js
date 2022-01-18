074df9098444c0992bcae34d982eef3fc3edf032
gustavogmail <gustavograal@gmail.com>
2022-01-17 22:43:36 -0300

    first commit

---
+++ ToDo_test.js
@@ -0,0 +1,8 @@
+Feature('ToDo');
+
+Scenario('test something', ({ I }) => {
+    I.amOnPage('/');
+    I.click('gustavogmail.github.io');
+    I.click('https://gustavogmail.github.io/react-ultimate-resume/')
+    I.see('Full-Stack Developer and QA Engineer');
+});
